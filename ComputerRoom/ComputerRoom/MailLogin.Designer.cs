0158435956f86e656b7d371b503f36caef16afde
coldean <jisung0603@gmail.com>
2023-05-19 16:41:22 +0900

    mail login 구현

--- ComputerRoom/ComputerRoom/MailLogin.Designer.cs
+++ ComputerRoom/ComputerRoom/MailLogin.Designer.cs
@@ -29,35 +29,84 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            txtPssword = new TextBox();
+            lblHint = new Label();
+            lblLogin = new Label();
+            boxLoginLogo = new PictureBox();
+            btnLogin = new Button();
+            txtPassword = new TextBox();
             txtID = new TextBox();
             label2 = new Label();
             label1 = new Label();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)boxLoginLogo).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = SystemColors.Control;
-            panel1.Controls.Add(txtPssword);
+            panel1.Controls.Add(lblHint);
+            panel1.Controls.Add(lblLogin);
+            panel1.Controls.Add(boxLoginLogo);
+            panel1.Controls.Add(btnLogin);
+            panel1.Controls.Add(txtPassword);
             panel1.Controls.Add(txtID);
             panel1.Controls.Add(label2);
             panel1.Controls.Add(label1);
-            panel1.Location = new Point(184, 122);
+            panel1.Location = new Point(184, 92);
             panel1.Name = "panel1";
-            panel1.Size = new Size(355, 125);
+            panel1.Size = new Size(355, 209);
             panel1.TabIndex = 0;
             // 
-            // txtPssword
+            // lblHint
             // 
-            txtPssword.Location = new Point(121, 64);
-            txtPssword.Name = "txtPssword";
-            txtPssword.Size = new Size(198, 27);
-            txtPssword.TabIndex = 3;
+            lblHint.AutoSize = true;
+            lblHint.ForeColor = Color.Red;
+            lblHint.Location = new Point(30, 150);
+            lblHint.Name = "lblHint";
+            lblHint.Size = new Size(82, 20);
+            lblHint.TabIndex = 7;
+            lblHint.Text = "힌트 : 점수";
+            lblHint.Visible = false;
+            // 
+            // lblLogin
+            // 
+            lblLogin.AutoSize = true;
+            lblLogin.ForeColor = Color.Red;
+            lblLogin.Location = new Point(30, 179);
+            lblLogin.Name = "lblLogin";
+            lblLogin.Size = new Size(64, 20);
+            lblLogin.TabIndex = 6;
+            lblLogin.Text = "lblLogin";
+            lblLogin.Visible = false;
+            // 
+            // boxLoginLogo
+            // 
+            boxLoginLogo.Location = new Point(120, 3);
+            boxLoginLogo.Name = "boxLoginLogo";
+            boxLoginLogo.Size = new Size(125, 62);
+            boxLoginLogo.TabIndex = 5;
+            boxLoginLogo.TabStop = false;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(224, 141);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Log In";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(120, 108);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(198, 27);
+            txtPassword.TabIndex = 3;
             // 
             // txtID
             // 
-            txtID.Location = new Point(121, 27);
+            txtID.Location = new Point(120, 71);
             txtID.Name = "txtID";
             txtID.Size = new Size(198, 27);
             txtID.TabIndex = 2;
@@ -65,7 +114,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(31, 71);
+            label2.Location = new Point(30, 115);
             label2.Name = "label2";
             label2.Size = new Size(72, 20);
             label2.TabIndex = 1;
@@ -74,7 +123,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(31, 30);
+            label1.Location = new Point(30, 74);
             label1.Name = "label1";
             label1.Size = new Size(24, 20);
             label1.TabIndex = 0;
@@ -91,15 +140,20 @@
             Text = "Mail";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)boxLoginLogo).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panel1;
-        private TextBox txtPssword;
+        private TextBox txtPassword;
         private TextBox txtID;
         private Label label2;
         private Label label1;
+        private PictureBox boxLoginLogo;
+        private Button btnLogin;
+        private Label lblLogin;
+        private Label lblHint;
     }
 }
--- ComputerRoom/ComputerRoom/MailLogin.cs
+++ ComputerRoom/ComputerRoom/MailLogin.cs
@@ -16,5 +16,32 @@ namespace ComputerRoom
         {
             InitializeComponent();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text == "freeman75" && txtPassword.Text == "50982")
+            {
+                mail ml = new mail();
+                //ml.Owner = this;
+                ml.Show();
+                this.Close();
+            }
+            else if (txtID.Text != "freeman75")
+            {
+                lblLogin.Text = "존재하지 않는 ID 입니다.";
+                lblLogin.Visible = true;
+                lblHint.Visible = false;
+                txtID.Clear();
+                txtPassword.Clear();
+            }
+            else if (txtID.Text == "freeman75" && txtPassword.Text != "50982")
+            {
+                lblLogin.Text = "비밀번호를 확인해 주시길 바랍니다.";
+                lblLogin.Visible = true;
+                lblHint.Visible = true;
+                txtID.Clear();
+                txtPassword.Clear();
+            }
+        }
     }
 }
